b775fd45c457b7a476ccaa776572cb2c12556dba
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-01-04 17:08:00 +0200

    hj

--- src/Auth/ResetPassword.jsx
+++ src/Auth/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../conestans/baseUrl";
-import { Stack } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 
 const ResetPassword = () => {
     const [password, setPassword] = useState("");
@@ -14,7 +14,7 @@ const ResetPassword = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch(`${BASE_URL}/api/reset-password?token=${token}`, {
+        const response = await fetch(`${BASE_URL}/user/reset-password?token=${token}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ newPassword: password }),
@@ -29,17 +29,18 @@ const ResetPassword = () => {
     };
 
     return (
-        <Stack sx={{ justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <form onSubmit={handleSubmit}>
-                <input
+        <Stack sx={{ justifyContent: 'center', alignItems: 'center', height: '100vh', padding: '10px' }}>
+            <Stack component='form' sx={{ width: '100%', gap: 2 }} onSubmit={handleSubmit}>
+                <TextField
                     type="password"
+                    fullWidth
                     placeholder="Enter new password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-                <button type="submit">Reset Password</button>
-            </form>
+                <Button type="submit" variant="contained">Reset Password</Button>
+            </Stack>
             {message && <p>{message}</p>}
         </Stack>
     );
